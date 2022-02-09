da35745223a7b87a9bfa4c7aa6d515b44012e558
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-02-09 17:56:19 +0000

    setting up sketch

--- public/sketch.js
+++ public/sketch.js
@@ -1,8 +1,19 @@
+let Engine = Matter.Engine,
+// Render = Matter.Render,
+World = Matter.World,
+Bodies = Matter.Bodies;
+
 function setup() {
   createCanvas(800, 400);
+  let options = {
+    isStatic: true
+  }
+  engine = Engine.create();
+  world = engine.world;
+  worm = Bodies.rectangle(400, 200, 80, 80, options);
+  Engine.run();
 }
 
 function draw() {
   background(51);
-  ellipse(mouseX, mouseY, 60, 60);
 }
