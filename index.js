4e881eb6d29cc29d33a2264d3dbe119030f5c303
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-02-10 13:55:45 +0000

    Added zoom feature

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 import p5 from 'p5';
 
-const sketch = (sk) => {    
-  sk.setup = () =>{
-    sk.createCanvas(800, 400);
+const sketch = (sk) => {
+  let sf = 1; // scaleFactor
+
+  sk.setup = () => {
+    sk.createCanvas(1200, 600);
   }
 
-  sk.draw = () =>{
-    sk.background(51);
+  sk.draw = () => {
+    mx = sk.mouseX;
+    my = sk.mouseY;
+    sk.translate(mx, my);
+    sk.scale(sf);
+    sk.translate(-mx, -my);
+
+
+    sk.background("#fae");
     sk.ellipse(sk.mouseX, sk.mouseY, 60, 60);
+    sk.rect(600, 300, 55, 55); // creating a test rectangle
+
   }
+
+  sk.keyPressed = () => {
+    if (sk.keyCode === sk.LEFT_ARROW) {
+      sf *= 1.10;
+    } else if (sk.keyCode === sk.RIGHT_ARROW) {
+      sf *= 0.90
+    }
+  }
+
+
 }
 
 const sketchInstance = new p5(sketch);
