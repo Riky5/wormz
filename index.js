93a733b739603947d41e898dcfad5f53d92f9f01
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-10 11:24:33 +0000

    added bundler for sketch

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const sketch = require('./public/sketch')
+import p5 from 'p5';
 
-console.log('test')
+const sketch = (sk) => {    
+  sk.setup = () =>{
+    sk.createCanvas(800, 400);
+  }
+
+  sk.draw = () =>{
+    sk.background(51);
+    sk.ellipse(sk.mouseX, sk.mouseY, 60, 60);
+  }
+}
+
+const sketchInstance = new p5(sketch);
