3f50ba5a1fefa730812b9dd6ca9f997d6f54c81b
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-02-12 15:17:36 +0000

    started abstracting sketch

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-import p5 from 'p5';
+const Sketch = require('./public/sketch')
 
-const sketch = (sk) => {    
-  sk.setup = () =>{
-    sk.createCanvas(800, 400);
-  }
-
-  sk.draw = () =>{
-    sk.background(51);
-    sk.ellipse(sk.mouseX, sk.mouseY, 60, 60);
-  }
-}
-
-const sketchInstance = new p5(sketch);
+Sketch.sketchWorld();
--- public/sketch.js
+++ public/sketch.js
@@ -1,62 +1,124 @@
 const { Engine, World, Bodies, Mouse, MouseConstraint, Constraint } = Matter;
+const p5 = require('p5');
 
-let world, engine;
-let ground;
-let worm;
-let worm2;
-let backgroundImg;
-let wormImg0;
-let wormImg1;
-let bullets;
-let mode;
-let mousePos;
-let moveLimit;
-let moveCount;
+class Sketch {
+  static sketchWorld() {
+    let world, engine;
+    let ground;
+    let worm;
+    let worm2;
+    let backgroundImg;
+    let wormImg0;
+    let wormImg1;
+    let bullets;
+    let mode;
+    let mousePos;
+    let moveLimit;
+    let moveCount;
 
-preload = () =>
-{
-  backgroundImg = loadImage("images/background-image.png");
-  wormImg0 = loadImage("images/worm0.png");
-  wormImg1 = loadImage("images/worm1.png");
+    const sketch = new p5 (function(p) {
+      p.preload = () =>
+      {
+        backgroundImg = p.loadImage("images/background-image.png");
+        wormImg0 = p.loadImage("images/worm0.png");
+        wormImg1 = p.loadImage("images/worm1.png");
+      }
+
+      p.setup = () => {
+        p.createCanvas(p.windowWidth, p.windowHeight - 50);
+        // initializeWorld(); requiresp
+        // Matter.Events.on(engine, "collisionStart", (event) => collision(event))
+        p.textSize(40);
+        mode = 'start';
+      }
+
+      p.draw = () => {
+        p.background('red')
+      }
+    }, "sketch")
+  } 
 }
 
-setup = () => {
-  createCanvas(windowWidth, windowHeight - 50);
-  initializeWorld();
-  Matter.Events.on(engine, "collisionStart", (event) => collision(event))
-  textSize(40);
-  mode = 'start';
-}
 
-draw = () => {
-  // method is in screenController.js
-  setScreen();
-}
 
-mouseClicked = () => {
-  // method is in controller.js
-  if(mode === 'game') {
-    Controller.fireBullet(); 
-  }
-}
+// class Sketch {
+//   static sketchWorld() {
+//     console.log("running....")
+//     const sketch = new p5 (function(p) {    
+//       console.log("running")
+//       p.preload = () =>
+//       {
+//         console.log("preload")
+//       }
+//    })
+//   }
+// }
 
-keyPressed = () => {
-  if (mode != 'game') {
-    // method is in screenController.js
-    screenControllerKeyPressed()
-  }
-  else {
+module.exports = Sketch;
 
-    let input = keyCode
+// let world, engine;
+// let ground;
+// let worm;
+// let worm2;
+// let backgroundImg;
+// let wormImg0;
+// let wormImg1;
+// let bullets;
+// let mode;
+// let mousePos;
+// let moveLimit;
+// let moveCount;
 
-    if(Controller.player1Turn === true) {
-      // method is in controller.js
-      MoveController.moveWorm(worm, input);
-    } 
-    else {
-      // method is in controller.js
-      MoveController.moveWorm(worm2, input);
-    }
-  }
-};
+// p.preload = () =>
+//       {
+//         backgroundImg = p.loadImage("images/background-image.png");
+//         wormImg0 = p.loadImage("images/worm0.png");
+//         wormImg1 = p.loadImage("images/worm1.png");
+//       }
+
+//       p.setup = () => {
+//         p.createCanvas(p.windowWidth, p.windowHeight - 50);
+//       }
+
+
+
+// setup = () => {
+//   createCanvas(windowWidth, windowHeight - 50);
+  // initializeWorld();
+  // Matter.Events.on(engine, "collisionStart", (event) => collision(event))
+  // textSize(40);
+  // mode = 'start';
+// }
+
+// draw = () => {
+//   // method is in screenController.js
+//   setScreen();
+// }
+
+// mouseClicked = () => {
+//   // method is in controller.js
+//   if(mode === 'game') {
+//     Controller.fireBullet(); 
+//   }
+// }
+
+// keyPressed = () => {
+//   if (mode != 'game') {
+//     // method is in screenController.js
+//     screenControllerKeyPressed()
+//   }
+//   else {
+
+//     let input = keyCode
+
+//     if(Controller.player1Turn === true) {
+//       // method is in controller.js
+//       MoveController.moveWorm(worm, input);
+//     } 
+//     else {
+//       // method is in controller.js
+//       MoveController.moveWorm(worm2, input);
+//     }
+//   }
+// };
 
