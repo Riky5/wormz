61a94d6ab617c08945811d7904608f978b04b1ff
riky moroni <riky5.moroni@gmail.com>
2022-02-13 20:33:13 +0000

    update server.js, package json

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.listen(port, '0.0.0.0');
-
 app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.listen(port, '0.0.0.0');
 
 console.log('Listening on port ' + port);
