facec6b8e068ea319e888a4ef1b52830a0c3c3b4
riky moroni <riky5.moroni@gmail.com>
2022-02-13 15:14:59 +0000

    restore server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const express = require('express')
 
 const app = express();
 app.use(cors());
-
-const port = server.listen(process.env.PORT || 3000);
-app.listen(port);
+app.listen(process.env.PORT || 3000);
 
 app.use(express.static('public/bundle.js'));
 
-console.log('Example app listening on port' + port);
+console.log('Example app listening on port 3000');
