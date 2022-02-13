399068bba2331483d6a092201ceab0866c6851f0
riky moroni <riky5.moroni@gmail.com>
2022-02-13 03:30:11 +0000

    add muisic/sound effects on/off buttons and sliders

--- public/controllers/screenController.js
+++ public/controllers/screenController.js
@@ -4,9 +4,9 @@ class ScreenController{
     background(wormsLogoImg);
     textSize(28);
     fill("#000000");
-    text(CENTER);
     text("Press ENTER to start game", windowWidth / 2 - 174, windowHeight / 2 + 110);
-    text("Press BACKSPACE for instructions", windowWidth / 2 - 220, windowHeight / 2 + 160);
+    text("Press BACKSPACE for instructions", windowWidth / 2 - 220, windowHeight / 2 + 150);
+    text("Press OPTION for Music/Sound settings", windowWidth / 2 - 252, windowHeight / 2 + 190);
   }
 
   static gameScreen() {
@@ -21,23 +21,30 @@ class ScreenController{
   }
 
   static gameOverScreen() {
-    background('#f9ebf9')
-    textSize(42)
-    text("GAME OVER", windowWidth / 2 - 140, windowHeight / 2 - 60)
-    textSize(30)
-    text("Press ENTER to go back to main page", windowWidth / 2 - 260, windowHeight / 2 + 10)
+    background('#f9ebf9');
+    textSize(42);
+    text("GAME OVER", windowWidth / 2 - 140, windowHeight / 2 - 60);
+    textSize(30);
+    text("Press ENTER to go back to main page", windowWidth / 2 - 260, windowHeight / 2 + 10);
   }
 
   static instructionsScreen() {
-    background('#f9ebf9')
-    textSize(32)
-    text("How to play:", windowWidth / 2 - 90, windowHeight / 3 - 140)
-    // textLeading(48)
+    background('#f9ebf9');
+    textSize(32);
+    text("How to play:", windowWidth / 2 - 90, windowHeight / 3 - 140);
     text("Use LEFT ◀️ and RIGHT ▶️ to move worm.", windowWidth / 2 - 310, windowHeight / 2 - 180);
-    text("Use UP 🔼 to jump.", windowWidth / 2 - 310, windowHeight / 2 - 110)
-    text("Aim and CLICK to shoot target 💥.",  windowWidth / 2 - 310, windowHeight / 2 - 40)
-    textSize(29)
-    text("Ready? Press ENTER to go back to main page", windowWidth / 2 - 307, windowHeight / 2 + 50)
+    text("Use UP 🔼 to jump.", windowWidth / 2 - 310, windowHeight / 2 - 110);
+    text("Aim and CLICK to shoot target 💥.",  windowWidth / 2 - 310, windowHeight / 2 - 40);
+    textSize(29);
+    text("Ready? Press ENTER to go back to main page", windowWidth / 2 - 307, windowHeight / 2 + 50);
+  }
+
+  static musicSoundScreen() {
+    resizeCanvas(0, 0);
+    music.setVolume(musicVolumeSlider.value());
+    startMusicBtn.mousePressed(startMusic);
+    stopMusicBtn.mousePressed(stopMusic);
+    backToMain.mousePressed(goToMain);
   }
 }
 
@@ -54,20 +61,42 @@ function setScreen() {
   else if (mode === 'instructions') {
     ScreenController.instructionsScreen();
   }
+  else if (mode === 'music-sound-settings') {
+    ScreenController.musicSoundScreen();
+  }
 }
 
 function screenControllerKeyPressed() {
   if(mode === 'start') {
-    if(keyCode ===ENTER) {
+    if(keyCode === ENTER) {
       mode = 'game';
     } 
     else if(keyCode === BACKSPACE) {
       mode = 'instructions';
     }
+    else if(keyCode === OPTION) {
+      mode = 'music-sound-settings'
+    }
   }
   else if(mode === 'gameOver' || mode === 'instructions') {
     if(keyCode === ENTER) {
-      setup();
+      goToMain();
     }
   }
+}
+
+startMusic = () => {
+  music.loop();
+}
+
+stopMusic = () => {
+  music.stop();
+}
+
+goToMain = () => {
+  createCanvas(windowWidth, windowHeight - 50);
+  initializeWorld();
+  Matter.Events.on(engine, "collisionStart", (event) =>collision(event))
+  textSize(40);
+  mode = 'start';
 }
--- public/sketch.js
+++ public/sketch.js
@@ -8,6 +8,11 @@ let wormsLogoImg;
 let backgroundImg;
 let wormImg0;
 let wormImg1;
+let music;
+let musicVolumeSlider;
+let startMusicBtn;
+let stopMusicBtn;
+let backToMain;
 let bullets;
 let mode;
 let mousePos;
@@ -20,10 +25,12 @@ preload = () =>
   backgroundImg = loadImage("images/background-image.png");
   wormImg0 = loadImage("images/worm0.png");
   wormImg1 = loadImage("images/worm1.png");
+  music = loadSound("assets/Whimsical-Popsicle.mp3");
 }
 
 setup = () => {
   createCanvas(windowWidth, windowHeight - 50);
+  initializeSound();
   initializeWorld();
   Matter.Events.on(engine, "collisionStart", (event) => collision(event))
   textSize(40);
@@ -33,6 +40,10 @@ setup = () => {
 draw = () => {
   // method is in screenController.js
   setScreen();
+}
+// automatically resize the window
+windowResized = () => {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 mouseClicked = () => {
@@ -62,3 +73,24 @@ keyPressed = () => {
   }
 };
 
+initializeSound = () => {
+  musicDiv = createDiv('');
+  createP('Music background:').parent(musicDiv);
+  startMusicBtn = createButton('ON').parent(musicDiv);
+  stopMusicBtn = createButton('OFF').parent(musicDiv);
+  musicVolumeDiv = createDiv('');
+  createP('Adjust music volume:').parent(musicVolumeDiv);
+  musicVolumeSlider = createSlider(0, 1, 0.5, 0.01);
+  musicVolumeSlider.parent(musicVolumeDiv);
+  soundDiv = createDiv('');
+  createP('Sound Effects:').parent(soundDiv);
+  createButton('ON').parent(soundDiv);
+  createButton('OFF').parent(soundDiv);
+  soundVolumeDiv = createDiv('');
+  createP('Adjust sound effects volume:').parent(soundVolumeDiv);
+  createSlider(0, 1, 0.5, 0.01).parent(soundVolumeDiv);
+  buttonDiv = createDiv('');
+  createP('Back to main page:').parent(buttonDiv);
+  backToMain = createButton('🔙');
+  backToMain.parent(buttonDiv);
+}
