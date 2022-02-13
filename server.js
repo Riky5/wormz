663882f4a3d8f88b9f5938d13b9bb70eeaff515d
riky moroni <riky5.moroni@gmail.com>
2022-02-13 14:53:42 +0000

    update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express')
 
 const app = express();
 app.use(cors());
-app.listen(process.env.PORT || 3000);
+
+const port = server.listen(process.env.PORT || 3000);
+app.listen(port);
 
 app.use(express.static('public/bundle.js'));
 
-console.log('Example app listening on port 3000!')
+console.log('Example app listening on port' + port);
