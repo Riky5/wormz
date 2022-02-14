26bf844bdf0a1b203b3e76638cdaa157f1a539c4
Giorgi Gutsaev <giorgi.gutsaevi@gmail.com>
2022-02-14 17:27:04 +0000

    Bullet class unit testing: check Bullet is instantiated with correct params and injected Matter in Bullet class constructor

--- public/controllers/shootingController.js
+++ public/controllers/shootingController.js
@@ -7,21 +7,21 @@ class ShootingController {
     this.bullet;
   }
 
-  static fireBullet(p, game, img){
+  static fireBullet(p, game, img) {
     let angleDeg;
-    if(game.player1Turn === true) {
-      let wormPos = {x: game.worm.body.position.x, y: game.worm.body.position.y }
+    if (game.player1Turn === true) {
+      let wormPos = { x: game.worm.body.position.x, y: game.worm.body.position.y }
       angleDeg = Math.atan2(wormPos.y - p.mouseY, wormPos.x - p.mouseX);
-      this.bullet = new Bullet({x: wormPos.x + 50, y: wormPos.y - 40, r: 15, game: game, img: img});
+      this.bullet = new Bullet({ x: wormPos.x + 50, y: wormPos.y - 40, r: 15, game: game, img: img, matter: Matter });
     }
     else {
-      let wormPos = {x: game.worm2.body.position.x, y: game.worm2.body.position.y }
+      let wormPos = { x: game.worm2.body.position.x, y: game.worm2.body.position.y }
       angleDeg = Math.atan2(wormPos.y - p.mouseY, wormPos.x - p.mouseX);
-      this.bullet = new Bullet({x: wormPos.x - 50, y: wormPos.y - 40, r: 15, game: game, img: img});
+      this.bullet = new Bullet({ x: wormPos.x - 50, y: wormPos.y - 40, r: 15, game: game, img: img, matter: Matter });
     }
 
     game.bullets.push(this.bullet);
-    Matter.Body.setVelocity(this.bullet.body,{x:(-p.cos(angleDeg))*30, y:-(p.sin(angleDeg))*30});
+    Matter.Body.setVelocity(this.bullet.body, { x: (-p.cos(angleDeg)) * 30, y: -(p.sin(angleDeg)) * 30 });
 
     game.changePlayerTurn();
     TimerController.resetTimer();
