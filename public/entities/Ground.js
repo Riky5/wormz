8bd62efd601defd3edefbcd46565e3e5b31dc89b
Chris Thomas <hereloss@googlemail.com>
2022-02-14 10:49:36 +0000

    World border added

--- public/entities/Ground.js
+++ public/entities/Ground.js
@@ -1,4 +1,4 @@
-class Water {
+class Lava {
   constructor(x,y,w,h) {
     this.body = Matter.Bodies.rectangle(x,y,w,h,{label: "water"});
     Matter.World.add(world, this.body)
@@ -11,7 +11,7 @@ class Water {
     const angle = this.body.angle;
     push()
     translate(pos.x,pos.y)
-    fill(0, 0, 255)
+    fill(255, 0, 0)
     rectMode(CENTER)
     rect(0,0, this.w, this.h);
     pop()
