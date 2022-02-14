a8b2acabb59dbb882934f6603d00a783d9d2704a
Chris Clement <chrisclement@Chriss-MacBook-Air.local>
2022-02-14 11:30:59 +0000

    Tests added for timerController. All passing

--- test/timerControllerTest.js
+++ test/timerControllerTest.js
@@ -1,10 +1,7 @@
 'use strict';
 
 const expect = require('chai').expect;
-const Game = require('../public/models/game');
-const Worm = require('../public/entities/worm');
 const TimerController = require('../public/controllers/timerController');
-const Ground = require('../public/entities/ground');
 
 describe('TimerController', () => {
 
@@ -18,24 +15,22 @@ describe('TimerController', () => {
     expect(timerController.timer).to.eq(0);
     done();
   });
-  // it('.increaseTimer increases timer value by 1', function(done) {
-  //   // Issue with using static methods in timerController. Leads to increaseTimer not being able to be
-  //   // called on instances of timerController. Using it on TimerController itself means what is returned
-  //   // is undefined.
-  //   timerController.increaseTimer();
-  //   expect(timerController.timer).to.eq(1);
-  //   done();
-  // });
-  // it('.resetTimer changes the timer value back to 0', function(done) {
-  //   timerController.increaseTimer();
-  //   timerController.increaseTimer();
-  //   expect(timerController.timer).to.eq(0);
-  //   done();
-  // });
-  // it('.timeLeftOnTurn calculates the correct time reamining', function(done) {
-  //   timerController.increaseTimer();
-  //   timerController.increaseTimer();
-  //   expect(timerController.timeLeftOnTurn()).to.eq(TimeController.timeLimit - 2);
-  //   done();
-  // });
+  it('.increaseTimer increases timer value by 1', function(done) {
+    timerController.increaseTimer();
+    expect(timerController.timer).to.eq(1);
+    done();
+  });
+  it('.resetTimer changes the timer value back to 0', function(done) {
+    timerController.increaseTimer();
+    timerController.increaseTimer();
+    timerController.resetTimer();
+    expect(timerController.timer).to.eq(0);
+    done();
+  });
+  it('.timeLeftOnTurn calculates the correct time reamining', function(done) {
+    timerController.increaseTimer();
+    timerController.increaseTimer();
+    expect(timerController.timeLeftOnTurn()).to.eq(timerController.timeLimit - 2);
+    done();
+  });
 });
