16454fdcd6a3db3ce6db03528a61cbb11e1c4719
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-15 11:23:16 +0000

    refactored weaponController

--- public/controllers/weaponController.js
+++ public/controllers/weaponController.js
@@ -2,16 +2,12 @@ class WeaponController {
   static validMoves = () => [49, 50, 51];
  
   static activeWormChangeWeapon = (activeWorm, input) => {
-    console.log(activeWorm.currentWeapon)
     activeWorm.changeWeapon(input - 48)
-    console.log(activeWorm.currentWeapon)
   }
 
   static isValidInput = (keyCode) => {
-    let validArray = WeaponController.validMoves()
-    return validArray.includes(keyCode);
+    return WeaponController.validMoves().includes(keyCode);
   }
-
 }
 
 module.exports = WeaponController;
