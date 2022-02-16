dc8144913a35c49700ea43a59a7dbba9e242563a
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-16 14:36:52 +0000

    refactored obstacle

--- public/entities/obstacle.js
+++ public/entities/obstacle.js
@@ -1,6 +1,6 @@
 class Obstacle {
-  constructor({x: x,y: y, w: w,h: h, world: world, matter: matter, imgs: img}) {
-    this.body = matter.Bodies.rectangle(x,y,w,h,{label: "ground"});
+  constructor({x: x, y: y, w: w, h: h, world: world, matter: matter, imgs: img}) {
+    this.body = matter.Bodies.rectangle(x, y, w, h, {label: 'ground'});
     matter.World.add(world, this.body);
     this.w = w;
     this.h = h;
@@ -10,19 +10,14 @@ class Obstacle {
   }
   show(p) {
     const pos = this.body.position;
-    const angle = this.body.angle;
+
+    // render obstacle
     p.push();
-    p.translate(pos.x,pos.y);
-
-    // p.fill(0, 179, 0);
-    // p.rectMode(p.CENTER);
-    // p.rect(0,0, this.w, this.h);
-
+    p.translate(pos.x, pos.y);
     p.imageMode(p.CENTER);
     p.image(this.img, 0, 0, this.w, this.h);
-
     p.pop();
   }
-} 
+}
 
 module.exports = Obstacle;
