69614f7422e2b00c32ffd679b27114e31755c2cd
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-16 11:54:48 +0000

    reverted how end of turn works

--- public/controllers/shootingController.js
+++ public/controllers/shootingController.js
@@ -17,9 +17,9 @@ class ShootingController {
     sound.play(); 
     Matter.Body.setVelocity(this.bullet.body,{x:(-p.cos(angleDeg))*this.bullet.velocity, y:-(p.sin(angleDeg))*this.bullet.velocity});
     setTimeout(() => {
-      game.changePlayerTurn();
       game.timer.resetTimer();
     },1000)
+    game.changePlayerTurn();
   }
 }
 
