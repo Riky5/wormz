4e4979f1ffd29f885d950b24d28e287fba8e0a5a
Chris Thomas <christhomas@Chriss-MacBook-Air.local>
2022-02-16 15:41:49 +0000

    Location of weapon edited

--- public/entities/weapon.js
+++ public/entities/weapon.js
@@ -1,16 +1,12 @@
 class WeaponImage {
   show = (p,x,y,direction) => {
     let angle = Math.atan2(y - p.mouseY, x - p.mouseX);
-    console.log(angle)
     p.fill(255);
     p.translate(x, y);
     p.rotate(angle);
-    console.log(x)
-    console.log(y)
-    console.log(angle)
     p.push();
     if (direction === 'left') {p.rect(0 - 28, 0 + 5, 40, 7);}
-    else {p.rect(0 - 20, 0 + 7, 40, 7);}
+    else {p.rect(0 - 25, 0 - 12, 40, 7);}
     p.pop();
   }
 }
