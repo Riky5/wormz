3ebddd06db666417be7a1c7099de342bfa2c8515
riky moroni <riky5.moroni@gmail.com>
2022-02-16 10:23:00 +0000

    remove console log

--- public/entities/ground.js
+++ public/entities/ground.js
@@ -1,7 +1,5 @@
 class Lava {
   constructor({x: x,y: y, w: w,h: h, world: world, matter: matter, img: img, gif: gif}) {
-    console.log('in lava')
-    console.log(gif)
     this.body = matter.Bodies.rectangle(x,y,w,h,{label: "lava"});
     matter.World.add(world, this.body);
     this.w = w;
